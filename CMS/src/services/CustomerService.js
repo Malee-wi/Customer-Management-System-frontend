97c7f73f14d1287f72ab7cf401e195298a9eba45
Malee-wi <maleeshanimanthi2000@gmail.com>
2026-04-29 02:59:33 +0530

    Add api service files for CRUD related api calls

---
+++ CMS/src/services/CustomerService.js
@@ -0,0 +1,15 @@
+import api from "./api";
+
+export const getCustomers = () => api.get("/customers");
+
+export const getCustomerById = (id) =>
+  api.get(`/customers/${id}`);
+
+export const createCustomer = (data) =>
+  api.post("/customers", data);
+
+export const updateCustomer = (id, data) =>
+  api.put(`/customers/${id}`, data);
+
+export const uploadExcel = (formData) =>
+  api.post("/customers/upload", formData);
